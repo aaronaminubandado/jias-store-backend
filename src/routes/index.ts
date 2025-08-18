1937b8328e6534c2dc743a73329285321a297758
Aaron Bandado <aaron.aminu1@gmail.com>
2025-08-18 23:22:08 +0300

    feat: add login route to auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import authRoutes from "@/routes/auth.routes";
 
 export const router = Router();
 
@@ -6,8 +7,7 @@ router.get("/health", (_req, res) => {
 	res.json({ ok: true, uptime: process.uptime() });
 });
 
-
 //Feature routers
-// router.use("/auth",authRouter);
+router.use("/auth", authRoutes);
 // router.use("/stores",storeRouter);
 // router.use("/products",productRouter);
