3e82b4f60811f47a3ef4f5a8d15ee508c24907e6
Aaron Bandado <aaron.aminu1@gmail.com>
2025-08-18 23:21:42 +0300

    feat: auth login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -29,3 +29,34 @@ export const register = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const login = async (req: Request, res: Response) => {
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user)
+			return res.status(401).json({
+				message: "Invalid email",
+				error: "Invalid credentials",
+			});
+
+		const isValid = await comparePassword(password, user.password);
+		if (!isValid)
+			return res.status(401).json({
+				message: "Wrong password",
+				error: "Invalid credentials",
+			});
+
+		const token = jwt.sign(
+			{ id: user._id, role: user.role },
+			env.JWT_SECRET,
+			{ expiresIn: "2h" }
+		);
+		res.json({ token, email: user.email, role: user.role });
+	} catch (err) {
+		res.status(500).json({
+			message: "Login failed",
+			error: "Server error",
+		});
+	}
+};
