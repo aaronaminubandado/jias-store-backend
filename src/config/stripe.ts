10f530ea442d6a4830f613317fb3c90065cccd0f
Aaron Bandado <aaron.aminu1@gmail.com>
2025-09-03 13:52:00 +0300

    feat: create stripe checkout object

---
+++ src/config/stripe.ts
@@ -0,0 +1,8 @@
+import Stripe from "stripe";
+import { env } from "@/config/env";
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+	apiVersion: "2025-07-30.basil",
+});
+
+export default stripe;
