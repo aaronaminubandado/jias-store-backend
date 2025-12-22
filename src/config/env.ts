d9194d174829f23508abc0c4f1e341d231769c75
Aaron Bandado <aaron.aminu1@gmail.com>
2025-12-22 15:09:03 +0200

    fix: fix inport alias

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,7 +11,7 @@ export const env = {
 	NODE_ENV: process.env.NODE_ENV || "development",
 	PORT: Number(process.env.PORT || 5000),
 	FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5137",
-	MONGODB_URI: required("MONGODB_URI", "mongodb://127.0.0.1:27017/jiastore"),
+	MONGODB_URI: process.env.MONGO_URI as string,
 	JWT_SECRET:
 		process.env.NODE_ENV === "production"
 			? required("JWT_SECRET")
